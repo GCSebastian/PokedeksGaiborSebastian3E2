699463ed45eeadea693ae8497219905f99a319f5
GCSebastian <juan.gaiborc.est@uets.edu.ec>
2024-02-01 15:15:38 -0500

    Pokedeks lista
    La pokedeks se a finalizado con exito y con todo funcional

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {PokeapiService} from './../Services/apiservice.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,61 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  pokemon:any
+  pokemon2:any
+  pokemon3:any
+  pokemon4:any
+  pokemonId: number=0;
+  itemId: number=0;
+  pokemonName: string="";
+  itemName: string="";
+  pokemons: any[]=[];
+  constructor(private api: PokeapiService) {}
 
-  constructor() {}
+  getPokemonDataID(id: number){
+    try{
+   
 
+      this.api.getPokemonID(id).subscribe((response => {
+       this. pokemon = response;
+      }));
+    }catch (error){
+      console.log(error);
+    }
+  }
+
+
+  getPokemonDataName(name: string){
+    try{
+   
+
+      this.api.getPokemonName(name).subscribe((response => {
+       this. pokemon2 = response;
+      }));
+    }catch (error){
+      console.log(error);
+    }
+  }
+
+  getItemDataID(id: number){
+    try{
+      this.api.getItemID(id).subscribe((response => {
+       this. pokemon3 = response;
+      }));
+    }catch (error){
+      console.log(error);
+    }
+  }
+
+  getItemDataName(name: string){
+    try{
+   
+
+      this.api.getItemName(name).subscribe((response => {
+       this. pokemon4 = response;
+      }));
+    }catch (error){
+      console.log(error);
+    }
+  }
 }
